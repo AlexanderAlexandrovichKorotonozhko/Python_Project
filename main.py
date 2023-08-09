6dc81012f6192df1220f3314dd484d6254435935
aak-1 <Korotonozhkoaa@yandex.ru>
2023-08-09 16:12:44 +0300

    окошко тест

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
-# This is a sample Python script.
+from PySimpleGUI import *
+from random import *
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+theme('Black')
+# обрабатываем нажатие на кнопку
+def update():
+    # получаем новое случайное число
+    r = randint(1,100)
+    # получаем доступ к текстовому элементу
+    text_elem = window['-text-']
+    # выводим в него текст с новым числом
+    text_elem.update("Результат: {}".format(r))
 
+# что будет внутри окна
+# первым описываем кнопку и сразу указываем размер шрифта
+layout = [[Button('Генерировать число', enable_events=True, key='-FUNCTION-', font='Any 10')], # Arial Any Helvetica
+        # затем делаем текст
+        [Text('Результат:',  key='-text-', font='Helvetica 10')]]
+# size=(250, 100),
+# рисуем окно
+window = Window('Генератор случайных чисел', layout, size=(350,100))
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# запускаем основной бесконечный цикл
+while True:
+    # получаем события, произошедшие в окне
+    event, values = window.read()
+    # если нажали на крестик
+    if event in (WIN_CLOSED, 'Exit'):
+        # выходим из цикла
+        break
+    # если нажали на кнопку
+    if event == '-FUNCTION-':
+        # запускаем связанную функцию
+        update()
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# закрываем окно и освобождаем используемые ресурсы
+window.close()
